52d78c00816d9ce9c91574bc69b6dff153c2f699
Priyankasridhara <57745806+Priyankasridhara@users.noreply.github.com>
2021-11-25 12:24:31 +0530

    Add files via upload

---
+++ firstHTML/uppercase.js
@@ -0,0 +1,20 @@
+﻿<!DOCTYPE HTML>  
+<html>  
+    <body>  
+        <form method="post">  
+    First Name  
+            <input type="text" name="fname"/>  
+            <input oninput="this.value = this.value.toUpperCase()" />
+    Last Name  
+            <input type="text" name="lname"/>  
+        
+        <script>
+        function upperCaseF(a){
+    setTimeout(function(){
+        a.value = a.value.toUpperCase();
+    }, 1);
+}
+</script>
+</form>  
+    </body>  
+</html>
