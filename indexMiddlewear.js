26edbf498239b7c8f446e023b74c3044f88f8e8a
Priyankasridhara <57745806+Priyankasridhara@users.noreply.github.com>
2022-01-14 22:02:51 +0530

    Add files via upload

---
+++ indexMiddlewear.js
@@ -0,0 +1,15 @@
+// JavaScript source code
+const express=require('express');
+const app=express();
+const members=require('./Members');
+const logger=(req,res,next)=>{
+	console.log(`${req.protocol}://${req.get('host')}${req.originalUrl}`)
+next();
+}
+app.use(logger);
+app.get('/api/members',(req,res)=>{
+res.json(members);
+});
+
+const PORT=process.env.PORT||5050;
+app.listen(PORT,()=>console.log('server started...'));
